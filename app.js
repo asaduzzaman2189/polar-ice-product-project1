7ca6b5da28973d21775a2f6770bb3cbb39f0a7a1
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-14 06:42:55 +0600

    polarbd-express-delivery-css-part-3-properly-work

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
-/* 
+
+// box(n) function korte hobe-
+
 document.getElementById("box-1").addEventListener("change", function () {
-  const itemQuantity = document.getElementById("quantity-1");
-  itemQuantity.innerText = this.value * 12;
+  const quantityText = document.getElementById("quantity-1");
+  const boxPrice = document.getElementById("box-price-1");
+  const itemQuantity = document.getElementById("total-quantity-1");
+  const itemPrice = document.getElementById("item-price-1");
+
+  const quantityValue = parseInt(quantityText.innerText);
+  itemQuantity.innerText = this.value * quantityValue;
+
+  const previousBoxPriceText = boxPrice.innerText;
+  const boxPriceValue = parseFloat(previousBoxPriceText);
 
   const totalItemPrice = parseFloat(this.value) * boxPriceValue;
   itemPrice.innerText = totalItemPrice;
 });
-
-const itemPrice = document.getElementById("item-price-1");
-const boxPrice = document.getElementById("box-price-1");
-
-const previousBoxPriceText = boxPrice.innerText;
-const boxPriceValue = parseFloat(previousBoxPriceText);
-
- */
-
-document.getElementById("box-1").addEventListener("change", function () {
-  const itemQuantity = document.getElementById("quantity-1");
-  itemQuantity.innerText = this.value * 12;
-
-  const totalItemPrice = parseFloat(this.value) * boxPriceValue;
-  itemPrice.innerText = totalItemPrice;
-});
-
-const itemPrice = document.getElementById("item-price-1");
-const boxPrice = document.getElementById("box-price-1");
-
-const previousBoxPriceText = boxPrice.innerText;
-const boxPriceValue = parseFloat(previousBoxPriceText);
