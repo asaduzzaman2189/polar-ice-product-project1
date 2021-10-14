211604385ce54714b99892aa461eefcc53927738
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-14 06:26:28 +0600

    polarbd-express-delivery-css-part-2

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-/* const requirementBox = parseFloat(document.getElementById("box-1").value);
-const previousQuantityItem = parseFloat(
-  document.getElementById("quantity-1").innerText
-);
-const updateQuantityItem.innerText= requirementBox*previousQuantityItem;
-console.log(updateQuantityItem); */
-
-/* document.getElementById("box-1").addEventListener("change", function () {
-    console.log("You selected: ", this.value);
-}); */
-
+/* 
 document.getElementById("box-1").addEventListener("change", function () {
-  requirementValue = this.value;
+  const itemQuantity = document.getElementById("quantity-1");
+  itemQuantity.innerText = this.value * 12;
 
-  const previousQuantityText = document.getElementById("quantity-1");
-  previousQuantityText.innerText = requirementValue * 12;
-  const totalItemQuantity = parseInt(previousQuantityText.innerText);
-  //   console.log(totalItemQuantity);
+  const totalItemPrice = parseFloat(this.value) * boxPriceValue;
+  itemPrice.innerText = totalItemPrice;
 });
 
 const itemPrice = document.getElementById("item-price-1");
-const previousItemPriceText = itemPrice.innerText;
-const previousItemPriceValue = parseFloat(previousItemPriceText);
-console.log(previousItemPriceValue);
-
 const boxPrice = document.getElementById("box-price-1");
-const previousBoxPriceText = boxPrice.innerText;
-const previousBoxPriceValue = parseFloat(previousBoxPriceText);
 
-const totalItemPrice = previousItemPriceValue + previousBoxPriceValue;
-// itemPrice.innerText = totalItemPrice;
-// console.log(itemPrice.innerText);
+const previousBoxPriceText = boxPrice.innerText;
+const boxPriceValue = parseFloat(previousBoxPriceText);
+
+ */
+
+document.getElementById("box-1").addEventListener("change", function () {
+  const itemQuantity = document.getElementById("quantity-1");
+  itemQuantity.innerText = this.value * 12;
+
+  const totalItemPrice = parseFloat(this.value) * boxPriceValue;
+  itemPrice.innerText = totalItemPrice;
+});
+
+const itemPrice = document.getElementById("item-price-1");
+const boxPrice = document.getElementById("box-price-1");
+
+const previousBoxPriceText = boxPrice.innerText;
+const boxPriceValue = parseFloat(previousBoxPriceText);
